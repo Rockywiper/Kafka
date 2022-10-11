8408075584f0fed9f73ce0d6552dc089326afb49
Prasanth.R <99342604+Rockywiper@users.noreply.github.com>
2022-10-11 17:44:06 +0530

    Add files via upload

---
+++ Kafka/src/main/java/com/kafka/KafkaController.java
@@ -0,0 +1,32 @@
+package com.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController 
+{
+	@Autowired
+	private KafkaTemplate<String, Object> temp;
+	
+	private String topic = "example";
+	
+	@GetMapping("/message/{name}")
+	public String msg(@PathVariable String name)
+	{
+		temp.send(topic, "Hi I am "+name);
+		return "Data Published";
+	}
+	
+	@GetMapping("/json")
+	public String msg()
+	{
+		Student student = new Student(1, "Prasanth", "IT");
+		temp.send(topic, student);
+		return "Json Data Published";
+	}
+	
+}
